3f1eccaa20bdae91b1425c6c360611a84e479109
djolker <dj.olker@gmail.com>
2014-10-31 03:03:36 -0500

    3:03am 8/31/2014
    Spawning sections in roughly correct areas

---
+++ ProceduralMapGen/Assets/MapControls.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapControls : MonoBehaviour {
+
+	public GameObject Hall;
+
+	public Vector3 lastPiece;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (GameObject.Find ("Start").collider.bounds.size);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			addHall();
+		}
+	}
+
+	void addHall()
+	{
+		if (lastPiece == null) 
+        {
+            lastPiece = GameObject.Find("Start").transform.position;
+            int x = 0;
+		}
+
+        Vector3 newPos = lastPiece;
+
+        newPos.z += 10;
+        Hall.transform.Translate(newPos);
+
+
+
+        Instantiate(Hall, newPos, Hall.transform.rotation);
+
+        lastPiece = newPos;
+	}
+}
